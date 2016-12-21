6a654c7427dae53bcb20fdc2d41bb6a638b32562
Christopher Manahan <chris.a.manahan@gmail.com>
2016-12-21 18:55:02 -0500

    built heapsort

--- sort.js
+++ sort.js
@@ -1,7 +1,6 @@
 /*------------- SORTING ALGORITHMS -------------*/
 
 /*------------- QUICKSORT -------------*/
-
 const quickSort = (arr) => {
   if (arr.length === 0) return arr;
   let part = partition(arr);
@@ -45,3 +44,40 @@ const mergeSort = (arr) => {
 const merge = (arr1, arr2) => {
 
 }
+
+/*------------- HEAPSORT -------------*/
+const heapSort = (arr) => {
+  let temp;
+  buildMaxHeap(arr);
+  for (let i = arr.length - 1; i > 0; i--) {
+    temp = arr[i];
+    arr[i] = arr[0];
+    arr[0] = temp;
+    arr.heapsize--;
+    maxHeapify(arr, 0)
+  }
+  return arr;
+}
+
+const maxHeapify = (arr, i) => {
+  let left = 2*i,
+      right = 2*i + 1,
+      max = i,
+      temp;
+
+  if (left <= arr.heapsize && arr[left] > arr[max]) max = left;
+  if (right <= arr.heapsize && arr[right] > arr[max]) max = right;
+  if(max !== i) {
+    temp = arr[max];
+    arr[max] = arr[i];
+    arr[i] = temp;
+    maxHeapify(arr, max);
+  }
+}
+
+const buildMaxHeap = (arr) => {
+  arr.heapsize = arr.length - 1;
+  for (let i = Math.floor(arr.length/2); i > -1; i--) maxHeapify(arr, i);
+}
+
+console.log(heapSort([9,3,2,1,4,10,14,15,8,7]));
